77e436a68fbeaa1dbf5a19d88957888a9b83ce8a
Jacob Stauch <31869991+JacobStauch@users.noreply.github.com>
2025-01-25 15:52:09 -0600

    Waypoints Part 1 - Spawn Abductees on Swimmer Bubble to Random Waypoints (#20)
    * Add waypoint and script to hold GameObject, update Swimmer to be able to spawn Abductees in random open waypoints
    
    * Add WaypointContainer and sample Waypoints to Main Scene

--- Assets/Scripts/Swimmer.cs
+++ Assets/Scripts/Swimmer.cs
@@ -1,13 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class Swimmer : MonoBehaviour
 {
     public GameObject timerPrefab;
-    public double desiredTimerValue = 120f;
     private GameObject timerInstance;
     private GameObject canvasObject;
-
-
+    public GameObject abducteePrefab;
+    private GameObject abducteeInstance;
+    public double desiredTimerValue = 120f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +31,8 @@ public class Swimmer : MonoBehaviour
         if (collider.gameObject.tag == "Bubble") {
             Debug.Log("Okay good it was the bubble");
             spawnTimer();
+            spawnAbducteeAtOpenWaypoint();
+            handleAbductedSwimmer();
         }
     }
 
@@ -35,5 +41,25 @@ public class Swimmer : MonoBehaviour
         timerInstance.transform.SetParent(canvasObject.transform);
         timerInstance.GetComponent<Timer>().sourceSwimmer = this.gameObject;
         timerInstance.GetComponent<Timer>().timerValue = desiredTimerValue;
+    }
+
+    private void spawnAbducteeAtOpenWaypoint() {
+        // Find open waypoint
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        GameObject[] openWaypoints = waypoints.Where(waypoint => waypoint.GetComponent<Waypoint>().isOpen()).ToArray();
+        //Vector2[] openWaypointCoords = openWaypoints.Select(
+        //    waypoint => new Vector2(waypoint.transform.position.x, waypoint.transform.position.x)).ToArray();
+        GameObject destWaypoint = openWaypoints[UnityEngine.Random.Range(0, openWaypoints.Length - 1)];
+        Vector2 destWaypointCoords = new Vector2(destWaypoint.transform.position.x, destWaypoint.transform.position.y);
+        // Spawn abudctee at waypoint and spawn in the correct location
+        abducteeInstance = Instantiate(abducteePrefab);
+        abducteeInstance.transform.SetParent(destWaypoint.transform);
+        abducteeInstance.transform.position = destWaypointCoords;
+        // tie it to the waypoint object so it will be considered inhabited next go around
+        destWaypoint.GetComponent<Waypoint>().inhabitedBy = abducteeInstance;
+    }
+
+    private void handleAbductedSwimmer() {
+        this.gameObject.SetActive(false);
     }
 }
