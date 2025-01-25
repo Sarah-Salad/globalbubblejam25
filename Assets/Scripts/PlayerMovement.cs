7d68b7c475b23153cf526fa1cd11fdad0fa054f3
Sarah Lester <sarahsalad@Sarahs-MacBook-Pro.local>
2025-01-25 13:37:12 -0600

    rotate player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,46 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-    public Rigidbody2D rigidbody2d;
-    private Vector2 moveInput;
+    public float rotationSpeed;
+    private Rigidbody2D _rigidbody2d;
+    private Vector2 _moveInput;
+    private Vector2 _moveInputSmoothed;
+    private Vector2 _moveInputVelocity;
 
-    private InputAction _moveAction;
-
-    private void Start()
+    void Start()
     {
-        _moveAction = InputSystem.actions.FindAction("Move");
+        _rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void FixedUpdate() 
     {
-        moveInput = _moveAction.ReadValue<Vector2>();
+        SetPlayerVelocity();
+        RotatePlayer();
+    }
 
-        moveInput.Normalize();
+    private void SetPlayerVelocity()
+    {
+        _moveInputSmoothed = Vector2.SmoothDamp(
+                    _moveInputSmoothed,
+                    _moveInput,
+                    ref _moveInputVelocity,
+                    0.1f);
 
-        rigidbody2d.linearVelocity = moveInput * moveSpeed;
+        _rigidbody2d.linearVelocity = _moveInputSmoothed * moveSpeed;
+    }
+    private void RotatePlayer()
+    {
+        if (_moveInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _moveInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            _rigidbody2d.MoveRotation(rotation);
+        }
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        _moveInput = inputValue.Get<Vector2>();
     }
 }
