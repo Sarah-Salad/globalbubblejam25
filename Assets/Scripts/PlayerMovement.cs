19ff21e6a7cc35ed0a8f7f55da4fb847367c234f
Jacob Stauch <31869991+JacobStauch@users.noreply.github.com>
2025-01-25 23:32:18 -0600

    Movement Iteration - Car Tank Controls + Boost (#28)
    * Car tank control type movement implementation
    
    * Add boost implementation
    
    * Set correct timer expiration on Player prefab on Main Scene as it had altered from the Prefab set values

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,98 +1,131 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
+using System;
+using System.Drawing.Drawing2D;
 
 public class PlayerMovement : MonoBehaviour
 {
     private static readonly int Moving = Animator.StringToHash("Moving");
-    
-    public float normalMoveSpeed;
-    public float dashSpeed;
-    public float dashLength = .5f, dashCooldown = 1f;
-    public float rotationSpeed;
-    private float _dashCounter;
-    private float _dashCooldownCounter;
-    private float _swimSpeed;
-    private Rigidbody2D _rigidbody2d;
-    private Vector2 _moveInput;
-    private Vector2 _moveInputSmoothed;
-    private Vector2 _moveInputVelocity;
-    private bool _isDashing = false;
     private Animator _animator;
-    
-    void Start()
-    {
-        _swimSpeed = normalMoveSpeed;
-        _rigidbody2d = GetComponent<Rigidbody2D>();
+
+    // Derived from the following tutorial https://www.youtube.com/watch?v=DVHcOS1E5OQ
+    public float driftFactor = 0.95f;
+    public float accelerationFactor = 3f;
+    public float turnFactor = 3.5f;
+    public float maxSpeed = 5f;
+    private float originalDriftFactor, originalAccelerationFactor, originalTurnFactor, originalMaxSpeed;
+
+    private float accelerationInput = 0;
+    private float steeringInput = 0;
+    private float rotationAngle = 0;
+
+    private float velocityVsUp = 0;
+    public float dashFactor = 2f;
+    public float dashTime = 1f;
+    private bool isDashing = false;
+    private Vector2 moveInput;
+
+    private Rigidbody2D rigidBody2D;
+
+    void Awake() {
+        rigidBody2D = this.GetComponent<Rigidbody2D>();
+        // Store copies of values set by default or via inspector so that
+        // once the dash is finished we can reset our values used in
+        // physics calculations
+        originalDriftFactor = driftFactor;
+        originalAccelerationFactor = accelerationFactor;
+        originalTurnFactor = turnFactor;
+        originalMaxSpeed = maxSpeed;
+    }
+
+    void Start() {
         _animator = GetComponent<Animator>();
     }
 
-    private void FixedUpdate() 
-    {
-        SetPlayerVelocity();
-        RotatePlayer();
-        if (_isDashing == true)
-        {
-            Dash();
+    void FixedUpdate() {
+        ApplyEngineForce();
+        
+        DampenOrthogonalVelocity();
+
+        ApplySteering();
+
+        _animator.SetBool(Moving, rigidBody2D.linearVelocity.magnitude > .1f);
+    }
+
+    private void OnMove(InputValue inputValue) {
+        moveInput = inputValue.Get<Vector2>();
+        accelerationInput = moveInput.y;
+        steeringInput = moveInput.x;
+    }
+
+    private void OnSprint(InputValue inputValue) {
+        Dash();
+    }
+
+    void ApplyEngineForce() {
+        velocityVsUp = Vector2.Dot(
+            transform.up, rigidBody2D.linearVelocity
+        );
+
+        if (velocityVsUp > maxSpeed && accelerationInput > 0 || accelerationInput < 0) {
+            // don't apply more engine force if above max speed or going backwards
+            return;
+        }
+
+        if (accelerationInput == 0) {
+            rigidBody2D.linearDamping = Mathf.Lerp(rigidBody2D.linearDamping, 3.0f, Time.fixedDeltaTime * 3);
+        }
+        else {
+            rigidBody2D.linearDamping = 0;
+        }
+
+        Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
+
+        rigidBody2D.AddForce(engineForceVector, ForceMode2D.Force);
+    }
+
+    void ApplySteering() {
+        rotationAngle -= steeringInput * turnFactor;
+
+        rigidBody2D.MoveRotation(rotationAngle);
+    }
+
+    void DampenOrthogonalVelocity() {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(
+            rigidBody2D.linearVelocity, transform.up
+        );
+
+        Vector2 rightVelocity = transform.right * Vector2.Dot(
+            rigidBody2D.linearVelocity, transform.right
+        );
+
+        rigidBody2D.linearVelocity = forwardVelocity + rightVelocity * driftFactor;
+    }
+
+    void Dash() {
+        if (!isDashing && rigidBody2D.linearVelocity != Vector2.zero) {
+            Debug.Log("Starting dash");
+            isDashing = true;
+            // boost 
+            driftFactor = 0.01f;
+            rigidBody2D.linearVelocity *= dashFactor;
+            turnFactor *= 2;
+            maxSpeed += dashFactor;
+            // cool down before reset vals
+            StartCoroutine(waitDashTime());
         }
     }
 
+    IEnumerator waitDashTime() {
+        Debug.Log("Waiting");
+        yield return new WaitForSeconds(dashTime);
 
-    private void SetPlayerVelocity()
-    {
-        _moveInputSmoothed = Vector2.SmoothDamp(
-                    _moveInputSmoothed,
-                    _moveInput,
-                    ref _moveInputVelocity,
-                    0.1f);
-
-        _rigidbody2d.linearVelocity = _moveInputSmoothed * _swimSpeed;
-        _animator.SetBool(Moving, _rigidbody2d.linearVelocity.magnitude > .1f);
-    }
-    private void RotatePlayer()
-    {
-        if (_moveInput != Vector2.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _moveInput);
-            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-            _rigidbody2d.MoveRotation(rotation);
-        }
-    }
-
-    private void OnMove(InputValue inputValue)
-    {
-        _moveInput = inputValue.Get<Vector2>();
-    }
-
-    private void OnSprint(InputValue inputValue)
-    {
-        _isDashing = true;
-    }
-
-    private void Dash()
-    {
-        if (_dashCooldownCounter <= 0 && _dashCounter <= 0)
-        {
-            _swimSpeed = dashSpeed;
-            _dashCounter = dashLength;
-        }
-
-        if (_dashCounter > 0) 
-        {
-            _dashCounter -= Time.deltaTime;
-
-            if (_dashCounter <= 0)
-            {
-                _swimSpeed = normalMoveSpeed;
-                _dashCooldownCounter = dashCooldown;
-                _isDashing = false;
-            }
-        }
-
-        if (_dashCooldownCounter > 0)
-        {
-            _dashCooldownCounter -= Time.deltaTime;
-        }
+        Debug.Log("Resetting vals");
+        isDashing = false;
+        driftFactor = originalDriftFactor;
+        accelerationFactor = originalAccelerationFactor;
+        turnFactor = originalTurnFactor;
+        maxSpeed = originalMaxSpeed;
     }
 }
